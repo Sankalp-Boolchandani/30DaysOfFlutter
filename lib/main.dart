291831e5900fddaf1f36c09060ca6e995f9c1143
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2021-06-13 17:50:37 +0530

    Day 2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/home_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Material(
-        child: Center(
-        child: Container(
-        child: Text("Hello world"),
-      )
-    )
-  ));
+    int days = 18;
+    return MaterialApp(home: HomePage());
   }
 }
