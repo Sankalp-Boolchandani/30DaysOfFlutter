8ba0ec0a1a09186cf0e3b3de039ef49e62e7e563
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2021-06-22 12:07:24 +0530

    themes extracted

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,8 +6,9 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Scaffold"),
-      ),
+          title: Text(
+        "Scaffold",
+      )),
       body: Center(
           child: Container(
         child: Text("Hello world"),
